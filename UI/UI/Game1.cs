df78b3481ae2dc42d29d76405d6e6381506c870f
Diogo Cardoso <amazing.red@gmail.com>
2011-11-11 14:03:51 +0000

    Added Model.cs

--- UI/UI/Game1.cs
+++ UI/UI/Game1.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using SpeechRecognitionDemo;
 using UI.Common;
 using UI.Geometry;
 using Microsoft.Xna.Framework;
@@ -14,6 +15,7 @@ using Microsoft.Research.Kinect.Nui;
 using UI.Kinect.Movement;
 using UI.Kinect.Movement.EventsArgs;
 using UI.Kinect.Movement.Gestures;
+using Model = Microsoft.Xna.Framework.Graphics.Model;
 
 namespace UI
 {
@@ -22,22 +24,35 @@ namespace UI
     /// </summary>
     public class Imaginect3D : Microsoft.Xna.Framework.Game
     {
-        GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
         SpriteFont spriteFont;
 
         private MovementTracker _mTracker;
 
         GeometricPrimitive _primitive;
-        bool _movedLeft, _movedRight, _movedUp, _movedDown, _movedBackward, _movedForward;
-
+       
         Runtime nui;
 
+
+        private bool _foundSkeleton;
+        Matrix world;
+        private RotateGesture _rotateGesture;
+        private TranslationGesture _translationGesture;
+        private ScaleGesture _scaleGesture;
+
+        private string speech = "";
+
+        public Matrix Projection { get; private set; }
+        public Matrix View { get; private set; }
+
+
+        SpeechRecognition _speechRecognition = new SpeechRecognition();
+        private GraphicsDeviceManager graphics;
 
         public Imaginect3D()
         {
             graphics = new GraphicsDeviceManager(this);
-           
+
             Content.RootDirectory = "Content";
         }
 
@@ -49,9 +64,55 @@ namespace UI
         /// </summary>
         protected override void Initialize()
         {
-            _primitive = new ConePrimitive(GraphicsDevice);
-            Components.Add(new FrameRateCounter(this));
+            View = Matrix.CreateLookAt(new Vector3(0, 0, 5f), Vector3.Zero, Vector3.Up);
+            Projection = Matrix.CreatePerspectiveFieldOfView(1, GraphicsDevice.Viewport.AspectRatio, 1, 6);
+
+            Geometry.Model model = new Geometry.Model(this);
+            Components.Add(model);
+
+            var conePrimitive = new ConePrimitive(GraphicsDevice);
+            var cubePrimitive = new CubePrimitive(GraphicsDevice);
+            var cylinderPrimitive = new CylinderPrimitive(GraphicsDevice);
+            var spherePrimitive = new SpherePrimitive(GraphicsDevice);
+            var torusPrimitive = new TorusPrimitive(GraphicsDevice);
+
+            model.AddPrimitives
+                (
+                    conePrimitive,
+                    cubePrimitive,
+                    cylinderPrimitive,
+                    spherePrimitive,
+                    torusPrimitive
+                );
             
+            Components.Add(new FrameRateCounter(this));
+            _speechRecognition.AddCommand(
+                new CommandSpeechRecognition("select cone", () =>
+                                                         {
+                                                             speech = "cone";
+                                                             _primitive = model.GetSingletonPrimitiveInstance<ConePrimitive>();
+
+                                                         }),
+                new CommandSpeechRecognition("select cube", () =>
+                                                    {
+                                                       speech = "cube";
+                                                       _primitive = model.GetSingletonPrimitiveInstance<CubePrimitive>();
+                                                    }),
+                new CommandSpeechRecognition("delete all", () =>
+                                                    {
+                                                        speech = "delete all"; model.ClearAll();
+                                                    }),
+                new CommandSpeechRecognition("add", () =>
+                                                        {
+                                                         speech = "Add";
+                                                        model.PrimitiveFreeze(_primitive, world, Color.Yellow);
+                                                        _primitive = null;
+                                                    })
+            );
+            /*/
+            _speechRecognition.InicializeSpeechRecognize();
+            _speechRecognition.Start();
+            //*/
             nui = Runtime.Kinects[0];
 
             try
@@ -63,65 +124,40 @@ namespace UI
 
             }
 
-            this.Exiting += (s, arg) => nui.Uninitialize();
+            Exiting += (s, arg) =>
+                           {
+                               nui.Uninitialize(); 
+                               _speechRecognition.Stop();
+                           };
 
             _mTracker = new MovementTracker(nui);
-            //_mTracker.AddMovementHandler(MovementType.Any, 0.01f, OnHandsChanged, JointID.HandRight);
-            //_mTracker.AddMovementHandler(MovementType.Any, 0.07f, OnRotateGesture, JointID.HandRight, JointID.HandLeft);
+            
             _rotateGesture = new RotateGesture(this, _mTracker);
             _translationGesture = new TranslationGesture(this, _mTracker);
             _scaleGesture = new ScaleGesture(this, _mTracker);
             _scaleGesture.Register();
-            //Components.Add(_translationGesture);
-            //Components.Add(_rotateGesture);
-            Components.Add(_scaleGesture);
-            //_rotateGesture.Register();
-            //_translationGesture.Register();
             
+            Components.Add(_translationGesture);
+            //Components.Add(_rotateGesture);
+            // Components.Add(_scaleGesture);
+            //_rotateGesture.Register();
+            _translationGesture.Register();
+
 
             _mTracker.OnSkeletonOnViewChange += UpdateSkeletonState;
             base.Initialize();
         }
 
-        private bool _startYRightRotation, _startYLeftRotation;
-
-        private void OnRotateGesture(object state, MovementHandlerEventArgs args)
-        {
-            if(args.Joint == JointID.HandRight)
-                _startYRightRotation = args.KinectCoordinates.Z < args.Skeleton.Joints[JointID.ShoulderRight].Position.Z - 0.3 && 
-                    args.KinectCoordinates.Z < args.Skeleton.Joints[JointID.HandLeft].Position.Z - 0.1;
-            else
-                _startYLeftRotation = args.KinectCoordinates.Z < args.Skeleton.Joints[JointID.ShoulderLeft].Position.Z - 0.3 &&
-                    args.KinectCoordinates.Z < args.Skeleton.Joints[JointID.HandRight].Position.Z - 0.1;
-
-            _args = args;
-        }
+        
 
         private void UpdateSkeletonState(object state, SkeletonOnViewEventArgs args)
         {
             _foundSkeleton = args.State == SkeletonOnViewType.Entered;
         }
 
-        private MovementHandlerEventArgs _args = new MovementHandlerEventArgs() { Movements = new MovementType[0] };
-        private Vector rightHandVector, leftHandVector;
+       
 
-
-        private void OnHandsChanged(object state, MovementHandlerEventArgs args)
-        {
-            if (args.Joint == JointID.HandRight)
-                rightHandVector = args.KinectCoordinates;
-        }
-
-        private bool _foundSkeleton;
-
-        TimeSpan _current;
-        TimeSpan _old;
         
-        private Vector3 ConvertRealWorldPoint(Vector position)
-        {
-            var returnVector = new Vector3 {X = position.X*10, Y = position.Y*10, Z = position.Z};
-            return returnVector;
-        }
 
         /// <summary>
         /// LoadContent will be called once per game and is the place to load
@@ -157,58 +193,48 @@ namespace UI
                 this.Exit();
 
 
-            _current = gameTime.TotalGameTime;
-            
-            if (_old == null)
-                _old = _current;
 
             base.Update(gameTime);
         }
 
-        Matrix world;
 
         /// <summary>
         /// This is called when the game should draw itself.
         /// </summary>
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
-        private float yRotation = 0.1f;
-
-        private RotateGesture _rotateGesture;
-        private TranslationGesture _translationGesture;
-        private ScaleGesture _scaleGesture;
 
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
             spriteBatch.Begin();
 
-            Vector3 translation;
-
+          
             if (_foundSkeleton)
             {
                 spriteBatch.DrawString(spriteFont, "Found Skeleton.", new Vector2(48, 48), Color.White);
-                //spriteBatch.DrawString(spriteFont, "LEngt : " + Extensions.ArraysToString(_args.Movements), new Vector2(48, 70), Color.White);
-                if(_args.Skeleton != null)
-                spriteBatch.DrawString(spriteFont, "Shoulder: " + _args.Skeleton.Joints[JointID.ShoulderRight].Position.Z + " hand: " + _args.KinectCoordinates.Z,  new Vector2(48, 70), Color.White);
+                spriteBatch.DrawString(spriteFont, "Speach: " + speech, new Vector2(48, 70), Color.White);
             }
 
 
 
             //world = Matrix.CreateTranslation(ConvertRealWorldPoint(rightHandVector));
-          //  if (_startYRightRotation)
-           //     yRotation += (float )gameTime.ElapsedGameTime.TotalMilliseconds * 0.001f;
-           // else if(_startYLeftRotation)
-           //     yRotation -= (float)gameTime.ElapsedGameTime.TotalMilliseconds * 0.001f;
+            //  if (_startYRightRotation)
+            //     yRotation += (float )gameTime.ElapsedGameTime.TotalMilliseconds * 0.001f;
+            // else if(_startYLeftRotation)
+            //     yRotation -= (float)gameTime.ElapsedGameTime.TotalMilliseconds * 0.001f;
 
 
             //world = Matrix.Multiply(Matrix.CreateRotationY(yRotation), Matrix.CreateTranslation(ConvertRealWorldPoint(rightHandVector)));
             //world = _rotateGesture.GetRotationMatrix();
             //world = Matrix.Multiply(world, _translationGesture.GetTranslationMatrix());
-            world = _scaleGesture.GetScaleMatrix();    
-            Matrix view = Matrix.CreateLookAt(new Vector3(0, 0, 5f), Vector3.Zero, Vector3.Up);
-            Matrix projection = Matrix.CreatePerspectiveFieldOfView(1, GraphicsDevice.Viewport.AspectRatio, 1, 6);
+            //world = _scaleGesture.GetScaleMatrix();    
+            world = _translationGesture.GetTranslationMatrix();
 
-            _primitive.Draw(world, view, projection, Color.Red);
+            //Matrix view = Matrix.CreateLookAt(new Vector3(0, 0, 5f), Vector3.Zero, Vector3.Up);
+            //Matrix projection = Matrix.CreatePerspectiveFieldOfView(1, GraphicsDevice.Viewport.AspectRatio, 1, 6);
+
+            if (_primitive != null)
+                _primitive.Draw(world, View, Projection, Color.Red);
 
             spriteBatch.End();
             base.Draw(gameTime);
---
+++ UI/UI/Geometry/Model.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace UI.Geometry
+{
+
+    internal class GeometricData
+    {
+        public Matrix Matrix { get; set; }
+        public Color Color { get; set; }
+    }
+    public class Model : DrawableGameComponent
+    {
+        private readonly Imaginect3D _game;
+    
+
+        IDictionary<GeometricPrimitive, List<GeometricData>> _models = new Dictionary<GeometricPrimitive, List<GeometricData>>();
+
+        public Model(Imaginect3D game)
+            : base(game)
+        {
+            _game = game;
+        }
+
+        public void AddPrimitives(params GeometricPrimitive[] primitives)
+        {
+            foreach (var geometricPrimitive in primitives)
+            {
+                _models.Add(geometricPrimitive, new List<GeometricData>());
+            }
+        }
+
+        public void PrimitiveFreeze(GeometricPrimitive primitive, Matrix location, Color color)
+        {
+            if (primitive == null)
+                return;
+            _models[primitive].Add(new GeometricData{Color = color, Matrix = location});
+        }
+
+        public override void Draw(GameTime gameTime)
+        {
+            foreach (var model in _models)
+            {
+                foreach (var data in model.Value)
+                {
+                    model.Key.Draw(data.Matrix,_game.View, _game.Projection,data.Color);
+                }
+            }
+            base.Draw(gameTime);
+        }
+
+        public GeometricPrimitive GetSingletonPrimitiveInstance<T>() where T : GeometricPrimitive
+        {
+            foreach (var geometricPrimitive in _models.Keys)
+            {
+                if (geometricPrimitive is T)
+                    return geometricPrimitive;
+            }
+            throw new InvalidOperationException();
+        }
+
+        public void ClearAll()
+        {
+            foreach (var model in _models)
+            {
+                model.Value.Clear();
+            }
+        }
+    }
+}
